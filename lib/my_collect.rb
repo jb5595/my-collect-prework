84841d15458f1e92d9515887c8cc85ca1d38735f
Jacob Berman <jacobberman1995@gmail.com>
2018-08-09 03:07:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(array) 
   i = 0
+  modified = []
   while i < array.length 
     element = array[i]
-    yield(element)
+    modified = yield(element)
     i += 1 
   end 
+  modified
 end 
